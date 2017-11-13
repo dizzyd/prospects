40e19acad881a88d76129ac34fa9deb033f8fd66
Azathoth <azathoth@cock.li>
2017-11-13 14:47:18 -0500

    add textures and localization

--- src/main/java/azathoth/util/prospecting/items/ProspectingItems.java
+++ src/main/java/azathoth/util/prospecting/items/ProspectingItems.java
@@ -1,5 +1,6 @@
 package azathoth.util.prospecting.items;
 
+import azathoth.util.prospecting.Prospecting;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 
@@ -10,13 +11,13 @@ public final class ProspectingItems {
 	public static Item sifting_pan;
 
 	public static final void init() {
-		prospecting_pan = new PanItem().setUnlocalizedName("prospecting_pan");
+		prospecting_pan = new PanItem().setUnlocalizedName("prospecting_pan").setTextureName(Prospecting.MODID + ":pan");
 		GameRegistry.registerItem(prospecting_pan, "prospecting_pan");
 
-		prospecting_pick = new PickItem().setUnlocalizedName("prospecting_pick");
+		prospecting_pick = new PickItem().setUnlocalizedName("prospecting_pick").setTextureName(Prospecting.MODID + ":pick");
 		GameRegistry.registerItem(prospecting_pick, "prospecting_pick");
 
-		sifting_pan = new SifterItem().setUnlocalizedName("sifting_pan");
+		sifting_pan = new SifterItem().setUnlocalizedName("sifting_pan").setTextureName(Prospecting.MODID + ":sifter");
 		GameRegistry.registerItem(sifting_pan, "sifting_pan");
 	}
 }
