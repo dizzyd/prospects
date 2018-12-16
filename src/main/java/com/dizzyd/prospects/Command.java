b0880e3cd75e3ff0115d2de70aebb59f7bf84275
Dave Smith <dizzyd@dizzyd.com>
2018-12-16 03:39:32 -0700

    Add "clear" command to facilitate debugging

--- src/main/java/com/dizzyd/prospects/Command.java
+++ src/main/java/com/dizzyd/prospects/Command.java
@@ -1,14 +1,27 @@
 package com.dizzyd.prospects;
 
 import com.dizzyd.prospects.world.WorldData;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.init.Blocks;
+import net.minecraft.network.play.server.SPacketChunkData;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerChunkMap;
+import net.minecraft.server.management.PlayerChunkMapEntry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.server.command.CommandTreeBase;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Command extends CommandTreeBase {
 
@@ -26,6 +39,7 @@ public class Command extends CommandTreeBase {
 		addSubcommand(new CommandOreInfo());
 		addSubcommand(new CommandShowConfig());
 		addSubcommand(new CommandReloadConfig());
+		addSubcommand(new CommandClearBlocks());
 	}
 
 	public static class CommandOreInfo extends CommandBase {
@@ -95,6 +109,63 @@ public class Command extends CommandTreeBase {
 		public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 			Prospects.config.reload();
 			Command.notifyCommandListener(sender, this, "cmd.prospects.reloadconfig.ok");
+		}
+	}
+
+	public static class CommandClearBlocks extends CommandBase {
+		@Override
+		public String getName() {
+			return "clear";
+		}
+
+		@Override
+		public String getUsage(ICommandSender sender) {
+			return "cmd.prospects.clear.usage";
+		}
+
+		@Override
+		public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+			int radius = CommandBase.parseInt(args[0]);
+			Block blockType = CommandBase.getBlockByText(sender, args[1]);
+
+			BlockPos playerPos = sender.getPosition();
+
+			World world = sender.getEntityWorld();
+			if (world.isRemote) {
+				return;
+			}
+
+			int count = 0;
+			Set<Chunk> chunks = new HashSet<Chunk>();
+
+			// Identify all block positions in the provided radius
+			Iterable<BlockPos> blockPositions = BlockPos.getAllInBox(playerPos.getX() - radius, playerPos.getY() - radius, playerPos.getZ() - radius,
+					playerPos.getX() + radius, playerPos.getY() + radius, playerPos.getZ() + radius);
+
+			// For each block in the radius, get the associated chunk, change the block state
+			// directly in chunk and save chunk for further processing. This side steps any
+			// neighbor notifications and allow us to avoid generating a large cascade of block updates
+			for (BlockPos p : blockPositions) {
+				IBlockState b = sender.getEntityWorld().getBlockState(p);
+				if (blockType.equals(b.getBlock())) {
+					Chunk chunk = world.getChunkFromBlockCoords(p);
+					chunk.setBlockState(p, Blocks.AIR.getDefaultState());
+					chunks.add(chunk);
+					count++;
+				}
+			}
+
+			// Walk over the set of chunks and generate a chunk refresh manually; this batches all
+			// the updates and ensures no block update notification
+			PlayerChunkMap manager = ((WorldServer) world).getPlayerChunkMap();
+			for (Chunk c : chunks) {
+				PlayerChunkMapEntry watcher = manager.getEntry(c.x, c.z);
+				if (watcher != null) {
+					watcher.sendPacket(new SPacketChunkData(c, -1));
+				}
+			}
+
+			Command.notifyCommandListener(sender, this, "cmd.prospects.clear.ok", count);
 		}
 	}
 }
